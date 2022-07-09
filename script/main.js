39138319e7fb5e99ddd43d998f34391126458105
liondot <51257576+liondot@users.noreply.github.com>
2022-07-09 19:40:27 +0900

    스크롤 애니메이션 구현 함수 생성

--- script/main.js
+++ script/main.js
@@ -1,94 +1,117 @@
- (()=> {
+ (() => {
 
-  let yOffset= 0; //window.pageYOffset 대신 쓸 변수
-  let prevScrollHeight = 0; //현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
-  let currentScene = 0; //현재 활성화된 (눈 앞에 보고있는) 씬(scroll-section)
+   let yOffset = 0; //window.pageYOffset 대신 쓸 변수
+   let prevScrollHeight = 0; //현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+   let currentScene = 0; //현재 활성화된 (눈 앞에 보고있는) 씬(scroll-section)
 
-  const sceneInfo = [
-    {
-      // 0
-      type: 'sticky',
-      heightNum: 5, //브라우저 높이의 5배로 scrollHeight 세팅
-      scrollHeight: 0,
-      objs: {
-        container: document.querySelector('#scroll-section-0')
-      }
-    },
-    {
-      // 1
-      type: 'normal',
-      heightNum: 5,
-      scrollHeight: 0,
-      objs: {
-        container: document.querySelector('#scroll-section-1')
-      }
-    },
-    {
-      // 2
-      type: 'sticky',
-      heightNum: 5,
-      scrollHeight: 0,
-      objs: {
-        container: document.querySelector('#scroll-section-2')
-      }
-    },
-    {
-      // 3
-      type: 'sticky',
-      heightNum: 5,
-      scrollHeight: 0,
-      objs: {
-        container: document.querySelector('#scroll-section-3')
-      }
-    }
-  ];
-
-  function setLayout() {
-    for(let i = 0; i<sceneInfo.length; i++) {
-      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
-    }
-   
-    yOffset = window.pageYOffset;
-    let totalScrollHeight = 0; 
-    for(let i = 0; i<sceneInfo.length; i++) {
-      totalScrollHeight += sceneInfo[i].scrollHeight;
-      if(totalScrollHeight >= yOffset) {
-        currentScene = i;
-        break
+   const sceneInfo = [{
+       // 0
+       type: 'sticky',
+       heightNum: 5, //브라우저 높이의 5배로 scrollHeight 세팅
+       scrollHeight: 0,
+       objs: {
+         container: document.querySelector('#scroll-section-0'),
+         messageA: document.querySelector('.message-a'),
+         messageB: document.querySelector('.message-b'),
+         messageC: document.querySelector('.message-c'),
+         messageD: document.querySelector('.message-d')
+       },
+       values: {
+         messageA_oppacity: [0, 1]
        }
-    }
-    document.body.setAttribute('id', `show-scene-${currentScene}`);
-  }
+     },
+     {
+       // 1
+       type: 'normal',
+       heightNum: 5,
+       scrollHeight: 0,
+       objs: {
+         container: document.querySelector('#scroll-section-1')
+       }
+     },
+     {
+       // 2
+       type: 'sticky',
+       heightNum: 5,
+       scrollHeight: 0,
+       objs: {
+         container: document.querySelector('#scroll-section-2')
+       }
+     },
+     {
+       // 3
+       type: 'sticky',
+       heightNum: 5,
+       scrollHeight: 0,
+       objs: {
+         container: document.querySelector('#scroll-section-3')
+       }
+     }
+   ];
 
-  function scrollLoop() {
-    prevScrollHeight = 0;
-    for(let i = 0; i < currentScene; i++) {
-      prevScrollHeight += sceneInfo[i].scrollHeight;
-    }
+   function setLayout() {
+     for (let i = 0; i < sceneInfo.length; i++) {
+       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+       sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+     }
 
-    if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
-      currentScene++;
-      document.body.setAttribute('id', `show-scene-${currentScene}`);
+     yOffset = window.pageYOffset;
+     let totalScrollHeight = 0;
+     for (let i = 0; i < sceneInfo.length; i++) {
+       totalScrollHeight += sceneInfo[i].scrollHeight;
+       if (totalScrollHeight >= yOffset) {
+         currentScene = i;
+         break
+       }
+     }
+     document.body.setAttribute('id', `show-scene-${currentScene}`);
+   }
 
-    } 
+   function playAnimation() {
+     switch (currentScene) {
+       case 0:
+        console.log('0 play');
+         break;
+       case 1:
+        console.log('1 play');
+         break;
+       case 2:
+        console.log('2 play');
+         break;
+       case 3:
+        console.log('3 play');
+         break;
+     }
+   }
 
-    if(yOffset < prevScrollHeight) {
-      if(currentScene === 0)
-      return;
-      currentScene--;
-      document.body.setAttribute('id', `show-scene-${currentScene}`);
+   function scrollLoop() {
+     prevScrollHeight = 0;
+     for (let i = 0; i < currentScene; i++) {
+       prevScrollHeight += sceneInfo[i].scrollHeight;
+     }
 
-    }
-      console.log(currentScene);
+     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+       currentScene++;
+       document.body.setAttribute('id', `show-scene-${currentScene}`);
+     }
 
-  }
-  
-  window.addEventListener('scroll', () => {
-    yOffset = window.pageYOffset;
-    scrollLoop()
-  })
+     if (yOffset < prevScrollHeight) {
+       if (currentScene === 0)
+         return;
+       currentScene--;
+       document.body.setAttribute('id', `show-scene-${currentScene}`);
 
-  window.addEventListener('resize', setLayout);
-  window.addEventListener('load', setLayout)
+     }
+
+     playAnimation();
+
+   }
+
+   window.addEventListener('scroll', () => {
+     yOffset = window.pageYOffset;
+     scrollLoop()
+   })
+
+   window.addEventListener('resize', setLayout);
+   window.addEventListener('load', setLayout)
  })();
