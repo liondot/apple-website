db1ee9bd788b5206d7138b6e524c398dc7dafbd6
yoon <funfunjulia@gmail.com>
2022-07-10 03:23:54 +0900

    텍스트 영역

--- script/main.js
+++ script/main.js
@@ -18,15 +18,29 @@
          messageD: document.querySelector('.message-d')
        },
        values: {
-         messageA_opacity: [0, 1 {
-           start: 0.1,
-           end: 0.2
-         }],
-         messageB_opacity: [0, 1 {
-           start: 0.4,
-           end: 0.4
-         }]
-       }
+				videoImageCount: 960,
+				imageSequence: [0, 959],
+				canvas_opacity_in: [0, 1, { start: 0, end: 0.1 }],
+				canvas_opacity_out: [1, 0, { start: 0.95, end: 1 }],
+				messageA_translateY_in: [20, 0, { start: 0.15, end: 0.2 }],
+				messageB_translateY_in: [30, 0, { start: 0.5, end: 0.55 }],
+				messageC_translateY_in: [30, 0, { start: 0.72, end: 0.77 }],
+				messageA_opacity_in: [0, 1, { start: 0.15, end: 0.2 }],
+				messageB_opacity_in: [0, 1, { start: 0.5, end: 0.55 }],
+				messageC_opacity_in: [0, 1, { start: 0.72, end: 0.77 }],
+				messageA_translateY_out: [0, -20, { start: 0.3, end: 0.35 }],
+				messageB_translateY_out: [0, -20, { start: 0.58, end: 0.63 }],
+				messageC_translateY_out: [0, -20, { start: 0.85, end: 0.9 }],
+				messageA_opacity_out: [1, 0, { start: 0.3, end: 0.35 }],
+				messageB_opacity_out: [1, 0, { start: 0.58, end: 0.63 }],
+				messageC_opacity_out: [1, 0, { start: 0.85, end: 0.9 }],
+				pinB_scaleY: [0.5, 1, { start: 0.5, end: 0.55 }],
+				pinC_scaleY: [0.5, 1, { start: 0.72, end: 0.77 }],
+				pinB_opacity_in: [0, 1, { start: 0.5, end: 0.55 }],
+				pinC_opacity_in: [0, 1, { start: 0.72, end: 0.77 }],
+				pinB_opacity_out: [1, 0, { start: 0.58, end: 0.63 }],
+				pinC_opacity_out: [1, 0, { start: 0.85, end: 0.9 }]
+			}
      },
      {
        // 1
@@ -58,9 +72,14 @@
    ];
 
    function setLayout() {
+    // 각 스크롤 섹션의 높이 세팅 
      for (let i = 0; i < sceneInfo.length; i++) {
-       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-       sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+       if(sceneInfo[i].type === 'sticky'){
+        sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+       }  else if (sceneInfo[i].type === 'nomal') {
+        sceneInfo[i].scrollHeight = sceneInfo[i].objs.container.offsetHeight + window.innerHeight * 0.5;;
+       }
+       sceneInfo[i].objs.container.style.height =  `${sceneInfo[i].scrollHeight}px`
      }
 
      yOffset = window.pageYOffset;
@@ -78,7 +97,7 @@
    function calcValues(values, currentYOffset) {
      let rv;
      // 현재 씬(스크롤섹션)에서 스크롤된 범위를 비율로 구하기 
-     const scrollHeight = scrollInfo[currentScene].scrollHeight;
+     const scrollHeight = sceneInfo[currentScene].scrollHeight;
      const scrollRatio = currentYOffset / scrollHeight;
 
      //  rv = scrollRatio * (values[1] - values[0] + values[0])
@@ -107,14 +126,23 @@
      const objs = sceneInfo[currentScene].objs;
      const values = sceneInfo[currentScene].values;
      const currentYOffset = yOffset - prevScrollHeight;
-
+     const scrollHeight = sceneInfo[currentScene].scrollHeight;
+     const scrollRatio = currentYOffset / scrollHeight;
 
 
      switch (currentScene) {
        case 0:
-         let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset)
-         console.log(messageA_opacity_in)
-         objs.messageA.style.opacity = messageA_opacity_in;
+        //  console.log(messageA_opacity_in)
+
+         if(scrollRatio <= 0.22) {
+          // in 
+         objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
+         objs.messageA.style.transform = `translateY(${calcValues(values.messageA_translateY_in, currentYOffset)}%)`;
+         } else {
+          // out 
+         objs.messageA.style.opacity = calcValues(values.messageA_opacity_out, currentYOffset);
+         objs.messageA.style.transform = `translateY(${calcValues(values.messageA_translateY_out, currentYOffset)}%)`;
+         }
          break;
        case 1:
          // console.log('1 play');
@@ -148,7 +176,6 @@
          return;
        currentScene--;
        document.body.setAttribute('id', `show-scene-${currentScene}`);
-
      }
 
      if (enterNewScene) return;
