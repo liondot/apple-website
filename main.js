df82d9587660f4d1c58dce6ed025a4542e11142e
yoon <funfunjulia@gmail.com>
2022-07-15 23:43:57 +0900

    currentScene 결정 함수

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 (() => {
+
+    let yOffset = 0; //pageYOffset대신 쓸 변수
+    let prevScrollHeight = 0; //현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 높이 값
+    let currentScene = 0; //현재 활성화된 (눈 앞에 보고 있는 )씬(scroll-section)
      
     const sceneInfo = [
         {
@@ -36,7 +40,7 @@
             objs: {
                 container: document.querySelector('#scroll-section-3')
             }
-        },
+        }, 
     ];
 
     function setLayout() {
@@ -45,11 +49,33 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
         }
-        console.log(sceneInfo)
+
+    }
+
+    function scrollLoop() {
+        prevScrollHeight = 0;
+
+        for(let i = 0; i < currentScene; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+
+        if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+        }
+
+        if(yOffset < prevScrollHeight) {
+            if(currentScene === 0) return;
+            currentScene--;
+        }
+
+        console.log(currentScene);
     }
 
     window.addEventListener('resize', setLayout);
-
+    window.addEventListener('scroll', ()=>{
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    });
     setLayout();
 
 })();
